56e8a29e3a783758bdc52e3ecd3b819f596422d8
dead-beef <dead-beef@users.noreply.github.com>
2017-07-29 06:15:38 +0000

    fix wildcard order

--- config/common.mk
+++ config/common.mk
@@ -17,13 +17,13 @@ uniq = $(if $1,$(firstword $1) $(call uniq,$(filter-out $(firstword $1),$1)))
 src-to-min = $(patsubst %.js,%.min.js,$(patsubst %.css,%.min.css,$1))
 
 define rwildcard
-	$(wildcard $1$2) \
-	$(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
+	$(sort $(wildcard $1$2)) \
+	$(foreach d,$(sort $(wildcard $1*)),$(call rwildcard,$d/,$2))
 endef
 
 define rwildcards
-	$(foreach p,$2,$(wildcard $1$p)) \
-	$(foreach d,$(wildcard $1*),$(call rwildcards,$d/,$2))
+	$(sort $(foreach p,$2,$(wildcard $1$p))) \
+	$(foreach d,$(sort $(wildcard $1*)),$(call rwildcards,$d/,$2))
 endef
 
 define prefix
