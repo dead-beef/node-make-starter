5f84be0c983f1aa3e08b38f6c4fd32cedda080b0
dead-beef <dead-beef@users.noreply.github.com>
2017-12-05 07:14:45 +0000

    move build directory variables

--- config/app.mk
+++ config/app.mk
@@ -2,11 +2,7 @@ LINT_ENABLED = 1
 LIBRARY =
 
 APP_NAME = app
-
 APP_DIR = src
-BUILD_DIR = build
-MIN_DIR := $(BUILD_DIR)/min
-DIST_DIR = dist
 
 LIB_NAME = vendor
 LIB_FONT_DIRS := $(RESOLVE_MATERIAL_DESIGN_ICONS_ICONFONT)/dist/fonts \
@@ -35,8 +31,6 @@ COPY_FILE_TYPES_WILDCARD := $(subst %,*,$(COPY_FILE_TYPES))
 COPY_FILES :=
 COPY_FILES += $(foreach d,$(COPY_DIRS),\
                           $(call rwildcards,$d/,$(COPY_FILE_TYPES_WILDCARD)))
-
-NPM_SCRIPTS =
 
 SERVER_IP := 127.0.0.1
 SERVER_PORT := 57005
