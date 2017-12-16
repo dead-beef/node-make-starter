f3881b39659cc9608cf732696d50fc29f8632a26
dead-beef <dead-beef@users.noreply.github.com>
2017-12-16 05:42:07 +0000

    remove makefiles

--- make/init.mk
+++
@@ -1,3 +0,0 @@
-MAKEFILE_DIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
-include $(MAKEFILE_DIR)/macros.mk
-$(eval $(call load-modules,commands vars))
--- make/min-json.js
+++
@@ -1,2 +0,0 @@
-const fs = require('fs');
-console.log(JSON.stringify(JSON.parse(fs.readFileSync(process.argv[2]))));
