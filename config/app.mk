1321c5f963dd86018b51869e54a5a4af37738409
dead-beef <dead-beef@users.noreply.github.com>
2017-12-25 16:48:37 +0000

    update makefiles

--- config/app.mk
+++ config/app.mk
@@ -1,35 +1,56 @@
+$(call load-modules,concat copy scss minify)
+
 LINT_ENABLED := 1
-LIBRARY :=
-LOAD_MODULES := js scss vendor
 
 APP_NAME := app
+VENDOR_NAME := vendor
 APP_DIR := src
 
-LIB_NAME := vendor
-LIB_FONT_DIRS := $(RESOLVE_MATERIAL_DESIGN_ICONS_ICONFONT)/dist/fonts \
-                 $(RESOLVE_MATERIALIZE_CSS)/dist/fonts/roboto
-LIB_FONT_DIST_DIRS := material-icons roboto
-LIB_FONT_TYPES := %.otf %.eot %.svg %.ttf %.woff %.woff2
-
 JS_DIRS := $(APP_DIR)/js
-JS_IGNORE := %.test.js
 JS_FILES := $(foreach d,$(JS_DIRS),$(call rwildcard,$d/,*.js))
+JS_FILES := $(APP_DIR)/umd/umd-start.js $(JS_FILES) $(APP_DIR)/umd/umd-end.js
 
-SCSS_DIRS = $(APP_DIR)/css
-SCSS_FILES = $(APP_DIR)/css/main.scss
-LIB_SCSS_FILES := $(APP_DIR)/css/vendor.scss
-LIB_SCSS_DEPS := $(APP_DIR)/css/_materialize.scss
+FONT_TYPES := *.otf *.eot *.svg *.ttf *.woff *.woff2
+COPY_TYPES = *.jpg *.jpe *.jpeg *.png *.gif *.svg *.ico *.html
 
-COPY_DIRS := $(APP_DIR)
-COPY_FILE_TYPES = %.jpg %.jpe %.jpeg %.png %.gif %.svg %.ico %.html
-COPY_FILE_TYPES_WILDCARD := $(subst %,*,$(COPY_FILE_TYPES))
-
-COPY_FILES :=
-COPY_FILES += $(foreach d,$(COPY_DIRS),\
-                          $(call rwildcards,$d/,$(COPY_FILE_TYPES_WILDCARD)))
+MAKEFILES += $(lastword $(MAKEFILE_LIST))
+WATCH_FILES += '$(APP_DIR)/**/*' '$(CONFIG_FILE)' '$(OVERRIDE_CONFIG_FILE)'
 
 SERVER_IP := 127.0.0.1
 SERVER_PORT := 57005
 
-MAKEFILES += $(lastword $(MAKEFILE_LIST))
-WATCH_FILES += '$(APP_DIR)/**/*' '$(CONFIG_FILE)'
+SERVER_START = http-server -a $(SERVER_IP) -p $(SERVER_PORT) -c-1 $(DIST_DIR)
+SERVER_STOP = -pkill -f http-server
+
+TEST_TARGETS := test test-watch test-e2e
+
+$(call concat-files,$(JS_FILES),$(DIST_DIR)/js/$(APP_NAME).js,CONCAT,LINTJS)
+$(call concat-files,$(VENDOR_JS_FILES),$(DIST_DIR)/js/$(VENDOR_NAME).js)
+
+$(call build-scss,$(APP_DIR)/css/$(APP_NAME).scss,$(DIST_DIR)/css)
+$(call build-scss,$(APP_DIR)/css/$(VENDOR_NAME).scss,$(DIST_DIR)/css)
+
+$(call copy-wildcards,$(COPY_TYPES),$(APP_DIR),$(DIST_DIR))
+
+$(call copy-wildcards,$(FONT_TYPES),\
+                      $(RESOLVE_MATERIAL_DESIGN_ICONS_ICONFONT)/dist/fonts,\
+                      $(DIST_DIR)/fonts/material-icons)
+
+$(call copy-wildcards,$(FONT_TYPES),\
+                      $(RESOLVE_MATERIALIZE_CSS)/dist/fonts/roboto,\
+                      $(DIST_DIR)/fonts/roboto)
+
+start: stop
+	$(call prefix,server,$(SERVER_START))
+
+stop:
+	$(call prefix,server,$(SERVER_STOP))
+
+test:
+	$(call prefix,test,karma start config/karma.conf.js --single-run)
+
+test-watch:
+	$(call prefix,test,karma start config/karma.conf.js)
+
+test-e2e:
+	$(call prefix,test,./e2e-test)
