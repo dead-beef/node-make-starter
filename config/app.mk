b8c80651f12f0a48d13c7aec75b160b39b8459b1
dead-beef <dead-beef@users.noreply.github.com>
2018-01-19 13:15:15 +0000

    update makefiles

--- config/app.mk
+++ config/app.mk
@@ -1,10 +1,11 @@
-$(call load-modules,concat copy scss minify)
+$(call load-modules,scss minify)
 
 LINT_ENABLED := 1
 
 APP_NAME := app
 VENDOR_NAME := vendor
 APP_DIR := src
+CONFIG_DIR := config
 
 JS_DIRS := $(APP_DIR)/js
 JS_FILES := $(foreach d,$(JS_DIRS),$(call rwildcard,$d/,*.js))
@@ -14,7 +15,9 @@ FONT_TYPES := *.otf *.eot *.svg *.ttf *.woff *.woff2
 COPY_TYPES = *.jpg *.jpe *.jpeg *.png *.gif *.svg *.ico *.html
 
 MAKEFILES += $(lastword $(MAKEFILE_LIST))
-WATCH_FILES += '$(APP_DIR)/**/*' '$(CONFIG_FILE)' '$(OVERRIDE_CONFIG_FILE)'
+
+WATCH_FILES := '$(APP_DIR)/**/*' '$(CONFIG_DIR)/**/*' package.json Makefile
+WATCH = chokidar $(WATCH_FILES) -i '**/.*' -c
 
 SERVER_IP := 127.0.0.1
 SERVER_PORT := 57005
@@ -22,10 +25,12 @@ SERVER_PORT := 57005
 SERVER_START = http-server -a $(SERVER_IP) -p $(SERVER_PORT) -c-1 $(DIST_DIR)
 SERVER_STOP = -pkill -f http-server
 
+LINT_JS = eslint $(1)
+
 TEST_TARGETS := test test-watch test-e2e
 
-$(call concat-files,$(JS_FILES),$(DIST_DIR)/js/$(APP_NAME).js,CONCAT,LINTJS)
-$(call concat-files,$(VENDOR_JS_FILES),$(DIST_DIR)/js/$(VENDOR_NAME).js)
+$(call build,$(JS_FILES),$(DIST_DIR)/js/$(APP_NAME).js, , ,LINT_JS)
+$(call build,$(VENDOR_JS_FILES),$(DIST_DIR)/js/$(VENDOR_NAME).js)
 
 $(call build-scss,$(APP_DIR)/css/$(APP_NAME).scss,$(DIST_DIR)/css)
 $(call build-scss,$(APP_DIR)/css/$(VENDOR_NAME).scss,$(DIST_DIR)/css)
@@ -40,11 +45,21 @@ $(call copy-wildcards,$(FONT_TYPES),\
                       $(RESOLVE_MATERIALIZE_CSS)/dist/fonts/roboto,\
                       $(DIST_DIR)/fonts/roboto)
 
+TARGETS += start stop watch min-watch
+
 start: stop
 	$(call prefix,server,$(SERVER_START))
 
 stop:
 	$(call prefix,server,$(SERVER_STOP))
+
+watch:
+	$(call prefix,build,-$(RESET_MAKE))
+	$(call prefix,watch,$(WATCH) '$(RESET_MAKE)')
+
+min-watch:
+	$(call prefix,build,-$(RESET_MAKE) min)
+	$(call prefix,watch,$(WATCH) '$(RESET_MAKE) min')
 
 test:
 	$(call prefix,test,karma start config/karma.conf.js --single-run)
